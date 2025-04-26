fee584fff0002418d90e5283427192d0d0de7781
yujeongJeon <workingnewjeong@gmail.com>
2025-04-27 03:43:31 +0900

    add webhook

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import {type Probot} from 'probot'
+import {run, type Probot} from 'probot'
 
-export default (app: Probot) => {
+function App(app: Probot) {
     app.on('issues.opened', async (context) => {
         const issueComment = context.issue({
             body: 'Thanks for opening this issue!',
@@ -8,12 +8,27 @@ export default (app: Probot) => {
         return context.octokit.issues.createComment(issueComment)
     })
 
+    app.on(['issues.edited'], async (context) => {
+        console.log(context.payload)
+        const issueComment = context.issue({
+            body: 'edited!',
+        })
+        return context.octokit.issues.createComment(issueComment)
+    })
+
     app.onError(async (error) => {
         app.log.error(error)
     })
+
+    process.on('SIGTERM', () => {
+        console.log('SIGTERM received, shutting down gracefully')
+        process.exit(0)
+    })
 }
 
-process.on('SIGTERM', () => {
-    console.log('SIGTERM received, shutting down gracefully')
-    process.exit(0)
+run(App).catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error(`Something went wrong`)
+    // eslint-disable-next-line no-console
+    console.error(error)
 })
